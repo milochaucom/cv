9e71488c42398c477991479a1e74ddabc0eeebbd
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-04-28 12:35:02 +0200

    Add missing components

---
+++ src/cv-client/src/components/app/section/AppSectionHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="text-center text-primary text-h6 mt-2 mb-4">
+    <slot />
+  </h2>
+</template>
--- src/cv-client/src/data/routes.ts
+++ src/cv-client/src/data/routes.ts
@@ -2,6 +2,9 @@ import type { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   { name: 'Home', path: '', component: () => import('@/pages/home/PageHome.vue') },
+  { name: 'Privacy', path: 'privacy', component: () => import('../pages/PagePrivacy.vue') },
+  { name: 'Forbidden', path: 'forbidden', component: () => import('../pages/PageForbidden.vue') },
+  { name: 'NotFound', path: ':pathMatch(.*)*', component: () => import('../pages/PageNotFound.vue') },
 ]
 
 export default routes
